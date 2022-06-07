0af582eb8921c53d32166885fcc82d2ff969b768
mbrhdrg01 <96500032+mbrhdrg01@users.noreply.github.com>
2022-06-08 02:00:12 +0530

    Update gdriveTools.py

--- bot/helper/drive_utils/gdriveTools.py
+++ bot/helper/drive_utils/gdriveTools.py
@@ -234,7 +234,7 @@ class GoogleDriveHelper:
                 msg += f'\n<b>Size: </b>{get_readable_file_size(self.transferred_size)}'
                 msg += f"\n<b>Type: </b>Folder"
                 msg += f"\n<b>SubFolders: </b>{self.total_folders}"
-                msg += f"\n<b>Files: </b>{self.total_files}"
+                msg += f"\n<b>Files: </b>{self.total_files}\n\n<b>📬 Bot Created By : #𝒁𝑲𝑷𝟏𝟒𝟑</b>"
                 # msg += f'\n\n<b><a href="{self.__G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(dir_id)}">Drive Link</a></b>'
                 
                 url = self.__G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(dir_id)
@@ -253,7 +253,7 @@ class GoogleDriveHelper:
                 msg += f'<b>Filename: </b><code>{file.get("name")}</code>'                                        
                 try:
                     msg += f'\n<b>Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
-                    msg += f'\n<b>Type: {typ}</b>\n\n📬 <b>Created By : ZKP143</b>'
+                    msg += f'\n<b>Type: {typ}</b>\n\n📬 <b>Bot Created By : 𝒁𝑲𝑷𝟏𝟒𝟑</b>'
                     # msg += f'\n\n<b><a href="{self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))}">Drive Link</a></b>'
                     url = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))
                     buttons.append([InlineKeyboardButton("Drive Link", url=url)])
