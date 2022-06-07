5b5deffc4f9adb11ac0fcf3d0e903884cc9159aa
mbrhdrg01 <96500032+mbrhdrg01@users.noreply.github.com>
2022-06-08 00:55:43 +0530

    Update parser.py

--- bot/helper/ext_utils/parser.py
+++ bot/helper/ext_utils/parser.py
@@ -101,3 +101,35 @@ def gdtot(url: str) -> str:
         return info['gdrive_link']
     else:
         raise DDLException(f"{info['message']}")
+
+
+def get_gp_link(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    res = client.head(url)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+
+    bs4 = BeautifulSoup(res.content, 'html.parser')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    time.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except:
+        return False
